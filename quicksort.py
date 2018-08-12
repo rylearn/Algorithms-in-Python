bfb4c549a9997c75994d94b8c946014c5da1eb73
rylearn <40655878+rylearn@users.noreply.github.com>
2018-08-11 21:44:12 -0700

    quicksort algo

---
+++ quicksort.py
@@ -0,0 +1,30 @@
+
+import random
+
+def partition(A, p, r):
+    pivot = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= pivot:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[r], A[i + 1] = A[i + 1], A[r]
+    return i + 1
+
+def randomized_partition(A, p, r):
+    i = random.randint(p, r)
+    A[r], A[i] = A[i], A[r]
+    return partition(A, p, r)
+
+def randomized_quicksort(A, p, r):
+    if p < r:
+        q = randomized_partition(A, p, r)
+        randomized_quicksort(A, p, q - 1)
+        randomized_quicksort(A, q + 1, r)
+
+def quicksort(A):
+    randomized_quicksort(A, 0, len(A) - 1)
+
+test_arr = [34, 32, 12, 143, 35, 21]
+quicksort(test_arr)
+print(test_arr)
