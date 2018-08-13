c94be076de13de8f50ff72fa29456f3963c13a64
rylearn <40655878+rylearn@users.noreply.github.com>
2018-08-13 16:42:48 -0700

    Rod cutting problem

---
+++ dynamic_programming/rod_cutting.py
@@ -0,0 +1,18 @@
+
+def memoized_cut_rod(p, n):
+    r = [0 for i in range(n + 1)]
+    s = [0 for i in range(n + 1)]
+    for i in range(1, n + 1):
+        q = float('-inf')
+        for j in range(1, i + 1):
+            if q < p[j - 1] + r[i - j]:
+                q = p[j - 1] + r[i - j]
+                s[i] = j
+        r[i] = q
+    return r, s
+
+prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+n = 10
+r, s = memoized_cut_rod(prices, 10)
+print(r)
+print(s)
