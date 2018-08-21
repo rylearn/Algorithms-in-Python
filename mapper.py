acbc504ac3a6f94870ede02872683515d04f77e8
rylearn <40655878+rylearn@users.noreply.github.com>
2018-08-20 23:02:23 -0700

    Map reduce simple version 1.0

---
+++ mapper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""A more advanced Mapper, using Python iterators and generators."""
+
+import sys
+
+def read_input(file):
+    for line in file:
+        yield line.split()
+
+def main(separator='\t'):
+    data = read_input(sys.stdin)
+    for words in data:
+        for word in words:
+            print('%s%s%d' % (word, separator, 1))
+
+if __name__ == "__main__":
+    main()
